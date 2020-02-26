771305c1eb65805f4500f77328b84920864cef2a
Thirupativandana <60088042+Thirupativandana@users.noreply.github.com>
2020-02-25 23:16:21 -0800

    Angular springwenrootdempApp

---
+++ spring-web-rest-boot-demo/src/test/java/com/cts/swrd/SpringWebRestBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.swrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRestBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
